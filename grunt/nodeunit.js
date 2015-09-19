a6a6e0f969b8ff8f7ba89b93b759bfa1b89cf31d
Kamil Piechaczek <pomek@desmart.com>
2015-09-20 00:17:58 +0200

    Grunt configuration

---
+++ grunt/nodeunit.js
@@ -0,0 +1,6 @@
+module.exports = function (grunt, options) {
+    return {
+        all: ['./test/**/*.test.js']
+    };
+};
+
---
+++ grunt/umd.js
@@ -0,0 +1,10 @@
+module.exports = function (grunt, options) {
+    return {
+        default: {
+            src: 'src/Container.js',
+            dest: 'dist/Container.js',
+            objectToExport: 'Container',
+        }
+    };
+};
+
